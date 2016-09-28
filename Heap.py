d9772963e5f85853628cac9295c1c078d5cea370
Iván Canales Martín <ivancanales@MacBook-Air-de-Ivan.local>
2016-09-28 20:33:55 +0200

    a ver si ahora si

--- Heap.py
+++ Heap.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Autor: canales
+# Autor: Iván Canales
 #
+##
 
-__all__ = ['min_heap']
+__all__ = ['min_heap', 'dict_heap']
 
 class Node:
     def __init__(self, key, data, c_funct = None):
@@ -50,7 +51,6 @@ class min_heap:
         Constructor del min_heap
         :param set_max: Permite establecer un tamaño inicial para el heap.
         """
-
         self._max = set_max
         self._vec = [None] * set_max
         self._last = -1;
@@ -90,7 +90,8 @@ class min_heap:
         self._vec[a] = self._vec[b]
         self._vec[b] = temp
 
-    def _upheap(self, i):
+    def _upheap(self):
+	i = self._last
         while i > 0:
             p = self._parent(i)
             if self._vec[i] < self._vec[p]:
@@ -99,23 +100,21 @@ class min_heap:
             else: i = 0
 
     def _downheap(self):
-        i = 0 # iniciamos la exploración en el primer elemento del heap
+        i = 0  # iniciamos la exploración en el primer elemento del heap
         while i < self._last:
-            f,r = self._children(i)
-            
+            f, r = self._children(i)
+
             if f <= self._last:
-                
+
                 m = (f if self._vec[f] < self._vec[r] else r) if r <= self._last else f
-                
+
                 if self._vec[i] > self._vec[m]:
-                    self._swap(i,m)
+                    self._swap(i, m)
                     i = m
                 else:
                     i = self._last
             else:
                 i = self._last
-
-
 
     """
     ====================
@@ -147,14 +146,32 @@ class min_heap:
         :param key:
         :param val:
         :return:
-        """
+	"""
         n = Node(key,val)
         if len(self._vec) < self._next() + 1: self._vec.append(n) # si arribem al final fem append
         else: self._vec[self._next()] = n # si no simplement asociem
         self._last += 1
-        self._upheap(self._last)
+        self._upheap()
 
-    def decrease_key(new_key, data):
+    def decrease_key(self, new_key, data):
+        """
+        Decrementa la llave de un elemento en el heap. Nota: el coste de este algoritmo es de O(n)
+        en el peor de los casos (pues contiene una exploracion lineal)
+        :param data:
+        :return:
+        """
+        i = 0
+        while i < self._last:
+            if self._vec[i].data == data:
+                if new_key > self._vec[i].key:
+                    raise Exception('La nueva llave debe ser más pequeña.')
+                else:
+                    self._vec[i].key = new_key
+                    self._upheap()
+            else:
+                i += 1
+
+    def increase_key(self, new_key, data):
         """
         Decrementa la llave de un elemento en el heap. Nota: el coste de este algoritmo es de O(n)
         en el peor de los casos (pues contiene una exploracion lineal)
@@ -162,12 +179,12 @@ class min_heap:
         :return:
         """
         while i < self._last:
-            if self._vec[i][1] == data:
-                if new_key > self._vec[i][0]:
-                    raise Exception('La nueva llave debe ser más pequeña.')
+            if self._vec[i].data == data:
+                if new_key > self._vec[i].key:
+                    raise Exception('La nueva llave debe ser más grande.')
                 else:
                     self._vec[i][0] = new_key
-                    self._upheap(i)
+                    self._downheap(i)
             else:
                 i += 1
 
@@ -183,4 +200,64 @@ class min_heap:
         return self._last + 1
 
     def __nonzero__(self):
-        return self._last == -1
+        return self._last != -1
+
+
+"""
+###########
+DICT HEAP
+###########
+"""
+class dict_heap(min_heap):
+    def __init__(self, set_max = 0):
+        """
+        Este heap incluye un diccionario en el que se guardan los valores de los nodos para acceder a su posicion.
+        Esto permite que el acceso al nodo 'decrease_key' sea constante~ i por lo tanto el coste total sea log(n)
+        :param set_max:
+        """
+        min_heap.__init__(self, set_max)
+        self.values = {}
+
+    #@overrides
+    def _swap(self, a, b):
+        temp = self._vec[a]
+        self.values[self._vec[a].data] = b
+        self.values[self._vec[b].data] = a
+
+        self._vec[a] = self._vec[b]
+        self._vec[b] = temp
+
+
+
+    #@overrides
+    def pop_min(self):
+        tmp = min_heap.pop_min(self)
+        self.values.pop(tmp.data, None)
+        return tmp
+
+
+    #@overrides
+    def insert(self,key, val):
+        self.values[val] = self._next()
+        min_heap.insert(self, key, val)
+
+    #@overrides
+    def decrease_key(self, new_key, data):
+        i = self.values[data]
+        if new_key > self._vec[i].key:
+            raise Exception('La nueva llave debe ser más pequeña.')
+        else:
+            self._vec[i].key = new_key
+            self._swap(self._last,i)
+	    self._upheap()
+
+    #@overrides
+    def increase_key(self,new_key, data):
+        i = self.values[data]
+        if new_key < self._vec[i].key:
+            raise Exception('La nueva llave debe ser más grande.')
+
+        else:
+            self._vec[i].key = new_key
+            self._swap(i, 0)
+            self._downheap()
