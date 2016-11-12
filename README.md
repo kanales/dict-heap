1719b0e6093766e6a880fce8ccdf8bf49ef00be1
kanales <icanalma7@alumnes.ub.edu>
2016-11-12 12:45:26 +0100

    Ahora implementa MutableMapping. Readme actualizado.

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
-# ESTRUCTURAS DE DATOS
 
-## dict\_heap
-En este modulo se incluye una versión de la estructura *cola prioritaria* implementada mediante un diccionario, que permite el acceso a los elementos en tiempo lineaj.
+# dict\_heap
+## ATENCIÓN: LA DOCUMENTACIÓN NO ESTA AL DÍA
+**dict\_heap** es una estructura de datos diseñada a partir de un diccionario. A diferencia de un diccionario tradicional los elementos estan ordenados siguiendo una estructura de heap binario, de manera que actua como una cola prioritaria con acceso constante a los elementos.
 
 ### Funciones
-* `min_heap(iterable, set_max, key)`: devuelve un objeto de tipo dict\_heap. Si se proporciona un iterador lo genera a partir del iterador con coste lineal. Con `key` se puede definir una funcion de comparacion.
+
+* `min_heap([iterable[, key[,default]]])`: devuelve un objeto de tipo dict\_heap. Si se proporciona un iterador lo genera a partir del iterador con coste lineal. Con `key` se puede definir una funcion de comparacion.
 * `peek()`: permite ver (sin eliminar) el elemento con la llave más pequeña del heap.
 * `pop()`: permite ver y eliminar el elememto con la llave más pequeña del heap.
-* `insert(key, item)`: permite insertar un elemento en el heap. Para usar el item como llave se puede usar el constructor con un elemento.
+* `insert(key[, item])`: permite insertar un elemento en el heap. Para usar el item como llave se puede usar el constructor con un elemento.
 
-### Costes de la operaciones
-* **insertar**: coste *O(log(n))* promedio.
-* **extraer**: coste *O(log(n))* promedio.
-* **modificar prioridad**: coste *O(log(n))* promedio.
-* **crear un heap**: coste *O(n)*.
+## Funciones heredadas de MutableMapping
+* `values()`: devuelve todos los valores del *dict\_heap*, i.e. los valores de la cola prioritaria.
+
+* `keys()`: devuelve todsa las llaves de los elementos de la cola prioritaria.
+
+* `items()`: devuelve todos los pares (llave, valor) de la cola prioritaria.
+
+* `__cmp__(dict)`:
+
+* `__contains__(item)`:
+
+* `__delitem__(key)`:
+
+* `__getitem__(data)`:
+
+* `__setitem__(data, new_key)`:
+
+* `__str__()`:
+
+* `__len__()`:
+
+* `__nonzero__()`:
+
+* `__iter__()`:
--- dict_heap.py
+++ dict_heap.py
@@ -3,30 +3,34 @@
 # Autor: Iván Canales
 # Version: 2.5
 ##
+from collections import MutableMapping
+import itertools
 
-class dict_heap(object):
-    __slots__ = ['_vec', '_last','_items']
+class dict_heap(MutableMapping):
+    __slots__ = ['_heap', '_last']
     """ CONSTRUCTOR """
     def __init__(self, iterable = None, default = None, key = None):
         """
-        Este heap incluye un diccionario en el que se guardan los valores de los nodos para acceder a su posicion.
-        Esto permite que el acceso al nodo 'decrease_key' sea constante~ i por lo tanto el coste total sea log(n).
+        Este heap incluye un diccionario en el que se guardan los valores de los
+        nodos para acceder a su posicion.  Esto permite que el acceso al nodo
+        'decrease_key' sea constante~ i por lo tanto el coste total sea log(n).
 
-        Cabe tener en cuenta que en este heap los elementos 'data' deben ser únicos, pues perteneceran a un diccionario'
-        :param set_max: Permite establecer un tamaño inicial para el heap.
+        Cabe tener en cuenta que en este heap los elementos 'data' deben ser
+        únicos, pues perteneceran a un diccionario' :param set_max: Permite
+        establecer un tamaño inicial para el heap.
         """
         if iterable:
             if default != None:
-                self._vec = [[default, i] for i in iterable]
+                self._heap = [[default, i] for i in iterable]
             elif default != None:
-                self._vec = [[key(i),i] for i in iterable]
+                self._heap = [[key(i),i] for i in iterable]
             else:
-                self._vec = [[i,i] for i in iterable]
-            self._last = len(self._vec) - 1
+                self._heap = [[i,i] for i in iterable]
+            self._last = len(self._heap) - 1
         else:
-            self._vec = [None]
+            self._heap = [None]
             self._last = -1;
-        self._items = {self._vec[i][1]:i for i in xrange(len(self))}
+        self.__dict__ = {self._heap[i][1]:i for i in xrange(len(self))}
         if self: self._build_heap()
 
     """ METODOS PRINCIPALES """
@@ -37,7 +41,7 @@ class dict_heap(object):
         :return: lista [key, data]
         """
         if self._last > -1:
-            return self._vec[0]
+            return self._heap[0]
         else:
             raise Exception('Heap is empty')
 
@@ -51,7 +55,7 @@ class dict_heap(object):
             self._swap(0, self._last)
             self._last -= 1
             self._downheap()
-            self._items.pop(tmp[1], None)
+            self.__dict__.pop(tmp[1], None)
             return tmp
         else:
             raise Exception('Heap is empty')
@@ -64,13 +68,15 @@ class dict_heap(object):
         :return:
         """
         item = item if item else key
-        if item in self._items:
+        if item in self.__dict__:
             raise Exception("El elemento " + item + " ya pertenece al heap")
         else:
-            self._items[item] = self._next()
+            self.__dict__[item] = self._next()
             n = [key,item]
-            if len(self._vec) < self._next() + 1: self._vec.append(n) # si arribem al final fem append
-            else: self._vec[self._next()] = n # si no simplement asociem
+            if len(self._heap) < self._next() + 1: 
+                self._heap.append(n) # si arribem al final fem append
+            else: 
+                self._heap[self._next()] = n # si no simplement asociem
             self._last += 1
             self._upheap()
 
@@ -79,18 +85,18 @@ class dict_heap(object):
         """
         Intercambia los nodos en posiciones 'a' y 'b'
         """
-        self._items[self._vec[a][1]] = b
-        self._items[self._vec[b][1]] = a
+        self.__dict__[self._heap[a][1]] = b
+        self.__dict__[self._heap[b][1]] = a
 
-        self._vec[a], self._vec[b] = self._vec[b], self._vec[a],
+        self._heap[a], self._heap[b] = self._heap[b], self._heap[a],
 
     def _decrease_key(self, data, new_key, i):
-        self._vec[i][0] = new_key
+        self._heap[i][0] = new_key
         self._swap(self._last, i)
         self._upheap()
 
     def _increase_key(self, data, new_key, i):
-        self._vec[i][0] = new_key
+        self._heap[i][0] = new_key
         self._swap(i, 0)
         self._downheap()
 
@@ -128,7 +134,7 @@ class dict_heap(object):
         i = self._last
         while i > 0:
             p = self._parent(i)
-            if self._vec[i][0] < self._vec[p][0]:
+            if self._heap[i][0] < self._heap[p][0]:
                 self._swap(i, p)
                 i = p
             else: i = 0
@@ -137,9 +143,9 @@ class dict_heap(object):
         l, r = self._children(i)
         min_ = i
 
-        if l <= self._last and self._vec[l][0] < self._vec[min_][0]:
+        if l <= self._last and self._heap[l][0] < self._heap[min_][0]:
             min_ = l
-        if r <= self._last and self._vec[r][0] < self._vec[min_][0]:
+        if r <= self._last and self._heap[r][0] < self._heap[min_][0]:
             min_ = r
 
         if min_ != i:
@@ -148,36 +154,53 @@ class dict_heap(object):
 
     def _modify_key(self, data, new_key):
         """
-        En el momento de modificar la key de un elemento del heap se deberia llamar esta función
+        En el momento de modificar la key de un elemento del heap se deberia
+        llamar esta función 
         """
         if data in self:
-            idx  = self._items[data]
-            if self._vec[idx][0] > new_key:
+            idx  = self.__dict__[data]
+            if self._heap[idx][0] > new_key:
                 self._decrease_key(data, new_key, idx)
-            elif self._vec[idx][0] <  new_key:
+            elif self._heap[idx][0] <  new_key:
                 self._increase_key(data, new_key, idx)
         else:
             raise Exception('Item does not exist.')
 
     """ SOBRECARGA DE METODOS """
+    def values(self):
+        return self.__dict__.keys()
+
+    def keys(self):
+        return (x[0] for x in self._heap)
+
+    def items(self):
+        return itertools.izip(self.keys(), self.values())
+
+    def __cmp__(self, dict):
+        return cmp(self.__dict__, dict)
 
     def __contains__(self, item):
         """
         Devuelve 'True' si el item item esta contenido en el heap
         """
-        return item in self._items
+        return item in self.__dict__
+
+    def __delitem__(self, key):
+        idx = self._dict__.pop(key, None)
+        del self._heap[idx]
 
     def __getitem__(self, data):
         """
         Devuelve la llave del elemento 'data'
         """
-        return self._vec[self._items[data]][0]
+        return self._heap[self.__dict__[data]][0]
 
     def __setitem__(self, data, new_key):
         self._modify_key(data, new_key)
 
     def __str__(self):
-        return "[" + ", ".join(str(self._vec[i][1]) for i in range(self._last +1)) + "]"
+        return ''.join("[", ", ".join(str(self._heap[i][1]) for i in
+            range(self._last +1))"]")
 
     def __len__(self):
         return self._last + 1
@@ -186,7 +209,7 @@ class dict_heap(object):
         return self._last != -1
 
     def __iter__(self):
-       return iter(self.d)
+        return iter(self.items())
 
 if __name__=='__main__':
     """ sanity check """
